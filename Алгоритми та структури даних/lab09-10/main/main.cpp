5faa0a8c6fb0119b9d197b869a32fc9865e67e15
Kushchevskyi Andrii <andrey.k.5678@gmail.com>
2024-02-09 16:55:04 +0200

    Add files via upload

---
+++ Алгоритми та структури даних/lab09-10/main/main.cpp
@@ -0,0 +1,549 @@
+﻿//Кущевский Андрей, ПЗ-19-1
+
+/*
+	Дано двовимірний масив дійсних чисел  a[1..n,1..m] (кількість рядків n та кількість стовпців m вводиться користувачем).
+	Впорядкувати за спаданням k-й та p-й рядки масиву використовуючи алгоритм шейкерного сортування (k та  p  вводяться користувачем).
+*/
+
+/*
+	Для відсортованих рядків матриці реалізувати алгоритм інтерполя-ційного пошуку введеного користувачем числа.
+*/
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+#define UP 72
+#define DOWN 80
+#define ENTER 13
+
+using namespace std;
+
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void hideCursor(bool show) {
+
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = show;
+	info.dwSize = 10;
+	SetConsoleCursorInfo(hStdOut, &info);
+
+}
+
+void gotoxy(short x, short y) {
+
+	SetConsoleCursorPosition(hStdOut, { x, y });
+
+}
+
+void normalText() {
+
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
+}
+
+void errorText() {
+
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_INTENSITY);
+
+}
+
+void clearCinBuff() {
+
+	cin.clear();
+	cin.sync();
+	cin.ignore(cin.rdbuf()->in_avail());
+
+}
+
+vector<int> indexForInterpolate;
+
+void sizeOfMatrix(int(&rows), int(&cols)) {
+
+	normalText();
+	hideCursor(true);
+
+	cout << "Введите количество строк:";
+	while (!(cin >> rows) || (rows <= 0)) {
+
+		clearCinBuff();
+		errorText();
+		cout << "Ошибка! Введите корректное значение:";
+
+	}
+	normalText();
+
+	cout << endl << "Введите количество столбцов:";
+	while (!(cin >> cols) || (cols <= 0)) {
+
+		clearCinBuff();
+		errorText();
+		cout << "Ошибка! Введите корректное значение:";
+
+	}
+	normalText();
+
+}
+
+void outputMatrix(float **matrix, int rows, int cols) {
+
+	system("cls");
+	normalText();
+	hideCursor(false);
+
+	if (rows == 0) {
+		cout << "В матрице отсутствуют значения!";
+		cout << endl << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	cout << "Матрица:" << endl;
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+
+			cout << setw(6) << matrix[i][j];
+
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+	system("pause");
+	system("cls");
+
+}
+
+void inputMatrix(float **(&matrix), int(&rows), int(&cols)) {
+
+	system("cls");
+	hideCursor(true);
+	normalText();
+
+	if (rows != 0) {
+
+		for (int i = 0; i < rows; ++i) {
+
+			delete matrix[i];
+
+		}
+
+		delete[] matrix;
+
+	}
+
+	sizeOfMatrix(rows, cols);
+
+	matrix = new float *[rows];
+	for (int i = 0; i < rows; ++i) {
+
+		matrix[i] = new float[cols];
+
+	}
+
+	cout << endl << endl;
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+
+			cout << "[" << i + 1 << "][" << j + 1 << "]:";
+			while (!(cin >> matrix[i][j])) {
+
+				clearCinBuff();
+				errorText();
+				cout << "Ошибка! Введите корректное значение:";
+
+			}
+			normalText();
+
+		}
+	}
+
+	cout << endl << endl;
+	hideCursor(false);
+	system("pause");
+	system("cls");
+
+}
+
+void cocktailSortSelectRow(float **(&matrix), int &widthOFMatrix, int &selectRow) {
+
+	float temp = 0;
+
+	int toLeft = 1;
+	int toRight = widthOFMatrix - 1;
+
+	while (toLeft <= toRight) {
+
+		for (int i = toRight; i >= toLeft; --i) {
+
+			if (matrix[selectRow][i - 1] > matrix[selectRow][i]) {
+
+				temp = matrix[selectRow][i];
+				matrix[selectRow][i] = matrix[selectRow][i - 1];
+				matrix[selectRow][i - 1] = temp;
+
+			}
+
+		}
+		++toLeft;
+
+		for (int i = toLeft; i <= toRight; ++i) {
+
+			if (matrix[selectRow][i - 1] > matrix[selectRow][i]) {
+
+				temp = matrix[selectRow][i];
+				matrix[selectRow][i] = matrix[selectRow][i - 1];
+				matrix[selectRow][i - 1] = temp;
+
+			}
+
+		}
+		--toRight;
+
+	}
+
+}
+
+void cocktailSortForMatrix(float **(&matrix), int(&rows), int(&cols)) {
+
+	system("cls");
+	hideCursor(true);
+	normalText();
+
+	if (rows == 0) {
+		hideCursor(false);
+		cout << "В матрице отсутствуют значения!";
+		cout << endl << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	cout << "Исходная матрица:" << endl;
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+
+			cout << setw(6) << matrix[i][j];
+
+		}
+		cout << endl;
+	}
+
+
+	int first = 0;
+	cout << endl << endl << "Введите первую строку, которую желаете отсортировать(k):";
+	while (!(cin >> first) || (first <= 0) || (first > rows)) {
+
+		clearCinBuff();
+		SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_INTENSITY);
+		cout << "Ошибка! Введите корректное значение:";
+
+	}
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
+	int second = 0;
+	cout << endl << "Введите второю строку, которую желаете отсортировать(p):";
+	while (!(cin >> second) || (second <= 0) || (second > rows)) {
+
+		clearCinBuff();
+		SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_INTENSITY);
+		cout << "Ошибка! Введите корректное значение:";
+
+	}
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
+	--first;
+	--second;
+
+	
+	cocktailSortSelectRow(matrix, cols, first);
+	cocktailSortSelectRow(matrix, cols, second);
+
+	cout << endl << endl << "Отсортированные строки матрицы:" << endl;
+	if (first != second) {
+	
+		cout << "[" << first + 1 << "]:";  for (int j = 0; j < cols; ++j) { cout << setw(5) << matrix[first][j]; };
+		cout << endl;
+		cout << "[" << second + 1 << "]:"; for (int j = 0; j < cols; ++j) { cout << setw(5) << matrix[second][j]; };
+	
+	}
+	else if (first == second) {
+
+		cout << "[" << first + 1 << "]:";  for (int j = 0; j < cols; ++j) { cout << setw(5) << matrix[first][j]; };
+
+	}
+
+	indexForInterpolate.push_back(first);
+	indexForInterpolate.push_back(second);
+
+	cout << endl << endl;
+	hideCursor(false);
+	system("pause");
+	system("cls");
+
+}
+
+float interpolateSearchInRow(float **(&matrix), int (&cols), int (&selectRows), float (&keyOfSearch)) {
+
+	//start intepolateSearch
+	float *a = new float[cols];
+
+	for (int i = 0; i < cols; ++i) {
+
+		a[i] = matrix[selectRows][i];
+
+	}
+
+	//объявляем необходимые локальные переменные
+	//изначально устанавливаем нижний индекс на начало массива,
+	//а верний на конец массива
+	int low = 0;
+	int high = cols - 1;
+	int mid;
+	//цикл интерполирующего поиска
+	while (a[low] < keyOfSearch && a[high] >= keyOfSearch)
+	{
+		//интерполирующий поиск производит оценку новой области поиска
+		//по расстоянию между ключом поиска и текущим значение элемента
+		mid = low + ((keyOfSearch - a[low]) * (high - low)) / (a[high] - a[low]);
+		//если значение в ячейке с индексом mid меньше, то смещаем нижнюю границу
+		if (a[mid] < keyOfSearch)
+			low = mid + 1;
+		//в случае, если значение больше, то смещаем верхнюю границу
+		else if (a[mid] > keyOfSearch)
+			high = mid - 1;
+		//если равны, то возвращаем индекс
+		else
+			return mid;
+	}
+	//если цикл while не вернул индекс искомого значения,
+	//то проверяем не находится ли оно в ячейке массива с индексом low,
+	//иначе возвращаем -1 (значение не найдено)
+	if (a[low] == keyOfSearch)
+		return low;
+	else
+		return -1;
+
+	delete[] a;
+
+}
+
+void interPolationSearch(float **(&matrix), int (&rows), int(&cols)) {
+
+	system("cls");
+	hideCursor(true);
+	normalText();
+
+	if (cols == 0) {
+		hideCursor(false);
+		cout << "В матрице отсутствуют значения!";
+		cout << endl << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	if (indexForInterpolate.empty()) {
+		hideCursor(false);
+		cout << "Не было найдено индексов отсортированных строк, выполните пункт меню \"Сортировка матрицы\"!";
+		cout << endl << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	
+	cout << "Матрица:" << endl;
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+
+			cout << setw(6) << matrix[i][j];
+
+		}
+		cout << endl;
+	}
+
+	int first = 0;
+	int second = 0;
+
+	cout << endl << "Строки, в которых будет совершён интерполяционный поиск:";
+	for (int i = 0; i < indexForInterpolate.size(); ++i) {
+		cout << indexForInterpolate[i] + 1 << ' ';
+		if (i == 0) {
+			first = indexForInterpolate[i];
+		}
+		else if (i == 1) {
+			second = indexForInterpolate[i];
+		}
+	}
+	cout << "!";
+	
+	cout << endl << endl << "Введите искомое значение:";
+	float findValues = 0;
+	while (!(cin >> findValues)) {
+
+		clearCinBuff();
+		errorText();
+		cout << "Ошибка! Введите корректное значение:";
+
+	}
+	normalText();
+
+	int result = 0;
+
+	if (first == second) {
+		result = interpolateSearchInRow(matrix, cols, first, findValues);
+		if (result != -1)
+			cout << "Значение было найдено в элементе под индексом [" << first + 1 << "][" << result + 1 << "]!" << endl;
+		else
+			cout << "Значение не было найдено!" << endl;
+
+	}
+	else if(first != second) {
+
+		result = interpolateSearchInRow(matrix, cols, first, findValues);
+		if (result != -1)
+			cout << "Значение было найдено в элементе под индексом [" << first + 1 << "][" << result + 1 << "]!" << endl;
+		else
+			cout << "Значение не было найдено!" << endl;
+
+		result = interpolateSearchInRow(matrix, cols, second, findValues);
+		if (result != -1)
+			cout << "Значение было найдено в элементе под индексом [" << first + 1 << "][" << result + 1 << "]!" << endl;
+		else
+			cout << "Значение не было найдено!" << endl;
+	}
+	
+	indexForInterpolate.clear();
+
+	cout << endl << endl;
+	hideCursor(false);
+	system("pause");
+	system("cls");
+
+}
+
+void mainMenu(float **(&matrix), int(&rows), int(&cols)) {
+
+	hideCursor(false);
+
+	string mainMenu[] =
+	{
+		"Просмотр состояния матрицы",
+		"Инициализация матрицы",
+		"Сортировка матрицы",
+		"Интерполяционный поиск",
+		"Выход из программы"
+	};
+	
+	int key = 0;
+	int activeMenu = 0;
+	short x;
+	short y;
+
+	while (true) {
+
+		x = 10, y = 5;
+
+		for (int i = 0; i < size(mainMenu); ++i) {
+			if (activeMenu == i) {
+				normalText();
+			}
+			else {
+				SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN);
+			}
+
+			gotoxy(x, ++y);
+			cout << mainMenu[i];
+
+		}
+
+		key = _getch();
+		switch (key) {
+
+		case UP: {
+			if (activeMenu > 0) {
+				--activeMenu;
+			}
+			break;
+		}
+
+		case DOWN: {
+			if (activeMenu < size(mainMenu) - 1) {
+				++activeMenu;
+			}
+			break;
+		}
+
+		case ENTER: {
+			switch (activeMenu) {
+
+			case 0: {
+				outputMatrix(matrix, rows, cols);
+				break;
+			}
+
+			case 1: {
+				inputMatrix(matrix, rows, cols);
+				break;
+			}
+
+			case 2: {
+				cocktailSortForMatrix(matrix, rows, cols);
+				break;
+			}
+			
+			case 3: {
+				interPolationSearch(matrix, rows, cols);
+				break;
+			}
+
+			case 4: {
+				system("cls");
+				cout << "Завершение работы программы..." << endl << endl;
+				system("pause");
+				return;
+			}
+
+					break;
+			}
+			break;
+		}
+
+		}
+
+	}
+
+}
+
+int main() {
+
+	setlocale(0, "rus");
+	SetConsoleTitle(L"Кущевский Андрей, ПЗ-19-1");
+
+	int rows = 0;
+	int cols = 0;
+
+	float **matrix = new float *[rows];
+	for (int i = 0; i < rows; ++i) {
+		matrix[i] = new float[cols];
+	}
+	normalText();
+
+	mainMenu(matrix, rows, cols);
+
+	for (int i = 0; i < rows; ++i) {
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+
+	return 0;
+
+}
---
+++ Алгоритми та структури даних/lab11-12/main/main/main.cpp
@@ -0,0 +1,454 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <vector>
+#include <iomanip>
+#define UP 72
+#define DOWN 80
+#define ENTER 13
+using namespace std;
+
+
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void clearCinBuff()
+{
+
+	cin.clear();
+	cin.sync();
+	cin.ignore(cin.rdbuf()->in_avail());
+
+}
+
+void endProcedure()
+{
+
+	clearCinBuff();
+	cout << endl << endl;
+	system("pause");
+	system("cls");
+
+}
+
+void gotoxy(short x, short y)
+{
+
+	SetConsoleCursorPosition(hStdOut, { x, y });
+
+}
+
+void hideCursor(bool showStatus)
+{
+
+	_CONSOLE_CURSOR_INFO info;
+	info.bVisible = showStatus;
+	info.dwSize = 20;
+	SetConsoleCursorInfo(hStdOut, &info);
+
+}
+
+void setColorText(short unsigned int item)
+{
+
+	SetConsoleTextAttribute(hStdOut, item);
+
+}
+
+//вузли АВЛ-дерева 
+struct Tree
+{
+	float info;
+	int h;
+	Tree* left;
+	Tree* right;
+};
+
+Tree* root = nullptr;
+
+//фукнция для проверки на пустоту дерева 
+bool isEmpty(Tree* (&Node))
+{
+
+	if (!Node)
+	{
+
+		return true;
+
+	}
+	else
+	{
+
+		return false;
+
+	}
+
+}
+
+// три допоміжні функції, пов'язані з висотою height, bfactor, fixheight
+
+//Перша є обгорткою для поля h, вона може працювати й з нульовими покажчиками
+//(з порожніми деревами). Вона повертає висоту дерева(піддерева):
+int height(Tree* p)
+{
+	if (p != NULL)return p->h;
+	else return 0;
+}
+
+//Друга обчислює balance factor заданого вузла (і працює тільки з ненульовими покажчиками):
+int bfactor(Tree* p)
+{
+	return height(p->right) - height(p->left);
+}
+
+//Третя функція відновлює коректне значення поля h заданого вузла
+//(за умови, що значення цього поля в правом і левом дочірніх вузлах є коректними):
+void fixheight(Tree* p)
+{
+	int h1 = height(p->left);
+	int h2 = height(p->right);
+	if (h1 > h2) p->h = h1 + 1;
+	else p->h = h2 + 1;
+}
+
+//правий простий поворіт
+Tree* rotateright(Tree* p) // правий поворот навколо p
+{
+	Tree* q = p->left;
+	p->left = q->right;
+	q->right = p;
+	fixheight(p);
+	fixheight(q);
+	return q;
+}
+
+//лівий простий поворіт
+Tree* rotateleft(Tree* q) // лівий поворот навколо q
+{
+	Tree* p = q->right;
+	q->right = p->left;
+	p->left = q;
+	fixheight(q);
+	fixheight(p);
+	return p;
+}
+
+//балансування вузлу бінарного дерева пошуку
+Tree* balance(Tree* p) // балансування вузла p
+{
+	fixheight(p);
+	if (bfactor(p) == 2)
+	{
+		if (bfactor(p->right) < 0)
+			p->right = rotateright(p->right);
+		return rotateleft(p);
+	}
+	if (bfactor(p) == -2)
+	{
+		if (bfactor(p->left) > 0)
+			p->left = rotateleft(p->left);
+		return rotateright(p);
+	}
+	return p;
+}
+
+Tree* create_avltree(Tree*&, float);//функція для створення бінарного дерева
+void search_tree(Tree*&, float);//функція пошуку ключового елементу
+void view_tree(Tree*&, int);//функція виводу бінарного дерева
+Tree* exit(Tree*&);//функція видалення бінарного дерева
+
+//вставкa нового вузла
+Tree* create_avltree(Tree*& p, float k)//функція створення БД(додавання одного елементу)
+{
+	if (p == NULL)
+	{
+		p = new Tree;
+		p->info = k;
+		p->h = 0;
+		p->left = NULL;
+		p->right = NULL;
+	}
+	else {
+		if (k < p->info) p->left = create_avltree(p->left, k);
+		else p->right = create_avltree(p->right, k);
+	}
+	return balance(p);
+}
+
+void search_tree(Tree*& tree, float k)//функція пошуку
+{
+	if (k == tree->info) { cout << "Елемент найден!"; return; }
+	if (k > tree->info) {
+		if (tree->right == NULL) cout << "Елемент не найден!";
+		else search_tree(tree->right, k);
+	}
+	else {
+		if (tree->left == NULL) cout << "Елемент не найден!";
+		else search_tree(tree->left, k);
+	}
+}
+
+void view_tree(Tree*& tree, int level)//функція виведення БД
+ //перевірка на існування БД або віток або листків цього БД
+{
+
+	if (tree != NULL) {
+		view_tree(tree->right, level + 3);
+		if (level != 0)for (int i = 0; i < level + 6; i++) cout << " ";
+		if (level == 0) cout << "Корень:";
+		cout << tree->info << endl;
+		view_tree(tree->left, level + 3);
+	}
+
+}
+
+Tree* exit(Tree*& tree)
+{//Видалення усього БД якщо воно існує
+
+	Tree* work = tree;
+	if (work->left != NULL) work->left = exit(work->left);
+	if (work->right != NULL) work->right = exit(work->right);
+	delete work;
+	return NULL;
+
+}
+
+//очистка выделенной динамической под дерево
+void clearTree(Tree* (&Node))
+{
+
+	//если в дереве отсутствуют ноды, прекращение работы функции
+	if (!Node) {
+		return;
+	}
+
+	clearTree(Node->left);
+	clearTree(Node->right);
+	delete Node;
+
+	Node = nullptr;
+
+	return;
+
+}
+
+void clearTreeMenu()
+{
+
+	system("cls");
+	setColorText(14);
+
+	if (isEmpty(root)) {
+		setColorText(12);
+		cout << "В дереве отсутствуют элементы, воспользуйтесь соответствующим пунктом меню для инициализации дерева!";
+		endProcedure();
+		return;
+	}
+
+	cout << "Очистка дерева...";
+	clearTree(root);
+
+	endProcedure();
+	return;
+
+}
+
+void mainMenu()
+{
+
+	hideCursor(false);
+
+	string mainMenu[] =
+	{
+		"Просмотр состояния дерева",
+		"Инициализация дерева значениями",
+		"Поиск элемента",
+		"Очистка дерева",
+		"Выход из программы"
+	};
+
+	short x = 0;
+	short y = 0;
+	int key = 0;
+	int activeMenu = 0;
+
+	while (true) {
+		hideCursor(false);
+
+		x = 10;
+		y = 3;
+
+		for (int i = 0; i < 5; ++i) {
+
+			if (activeMenu == i) {
+				setColorText(13);
+			}
+			else {
+				setColorText(14);
+			}
+
+			gotoxy(x, ++y);
+			cout << mainMenu[i];
+
+		}
+
+		key = _getch();
+
+		switch (key) {
+
+		case UP: {
+			if (activeMenu > 0) {
+				--activeMenu;
+			}
+			else {
+				activeMenu = 5 - 1;
+			}
+
+			break;
+		}
+		case DOWN: {
+
+			if (activeMenu < 5 - 1) {
+				++activeMenu;
+			}
+			else {
+				activeMenu = 0;
+			}
+
+			break;
+		}
+
+		case ENTER: {
+			switch (activeMenu) {
+			case 0: {
+
+				system("cls");
+				//Перевірка на існування дерева
+				if (root != NULL) {
+					view_tree(root, 0);
+					endProcedure();
+					system("cls");
+				}
+				else {
+					setColorText(12);
+					cout << "В дереве отсутствуют элементы, воспользуйтесь соответствующим пунктом меню для инициализации дерева!";
+					endProcedure();
+				}
+				break;
+			}
+
+			case 1: {
+				system("cls");
+				hideCursor(true);
+
+				if (!root) {
+					setColorText(12);
+					cout << "На данный момент в дереве отсутствуют элементы!" << endl << endl;
+					setColorText(14);
+				}
+				else {
+					setColorText(12);
+					cout << "На данный момент в дереве присутствуют элементы!" << endl << endl;
+					setColorText(14);
+				}
+
+				cout << "Введите количество элементов, которые желаете добавить:";
+				int size = 0;
+				while (!(cin >> size) || (size <= 0)) {
+
+					setColorText(12);
+					clearCinBuff();
+					cout << "Ошибка! Введите корректное значение:";
+
+				}
+
+				setColorText(14);
+				cout << endl;
+
+				float value = 0;
+
+				for (int i = 0; i < size; ++i) {
+
+					cout << "[" << i + 1 << "]:";
+					while (!(cin >> value)) {
+
+						setColorText(12);
+						clearCinBuff();
+						cout << "Ошибка! Введите корректное значение для [" << i + 1 << "]:";
+
+					}
+					setColorText(14);
+
+					root = create_avltree(root, value);
+
+				}
+
+				hideCursor(false);
+				cout << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
+
+			case 2: {
+				if (root != NULL) {
+					system("cls");
+					setColorText(14);
+					float key = 0;
+					hideCursor(true);
+					cout << "Введите значение, которе желаете найти:";
+					while (!(cin >> key)) {
+
+						setColorText(12);
+						clearCinBuff();
+						cout << "Ошибка! Введите корректное значение:";
+					}
+					hideCursor(false);
+					setColorText(14);
+					search_tree(root, key);
+					endProcedure();
+				}
+				else {
+					setColorText(12);
+					system("cls");
+					cout << "В дереве отсутствуют элементы, воспользуйтесь соответствующим пунктом меню для инициализации дерева!";
+					endProcedure();
+				}
+				break;
+			}
+
+			case 3: {
+				clearTreeMenu();
+				break;
+			}
+
+			case 4: {
+				system("cls");
+				setColorText(14);
+				system("pause");
+				return;
+			}
+
+			}
+			break;
+		}
+
+		}
+
+	}
+
+}
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	SetConsoleTitle(L"Кущевский Андрей, ПЗ-19-1");
+
+	mainMenu();
+	clearTree(root);
+
+	return 0;
+
+}
